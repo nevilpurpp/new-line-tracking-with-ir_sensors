b6f594dbd15735ff88d0e467e380a39067f6f331
NEVIL INGUTU <79148798+nevilpurpp@users.noreply.github.com>
2024-06-09 17:30:03 +0000

    Update definitions.py

--- definitions.py
+++ definitions.py
@@ -1,3 +1,13 @@
+
+
+mkdir /home/pi/.config/autostart
+nano /home/pi/.config/autostart/clock.desktop
+
+[Desktop Entry]
+Type=Application
+Name=Clock
+Exec=/usr/bin/python3 /home/pi/clock.py
+
 import RPi.GPIO as GPIO
 import time
 
